712cd70da7ca49bc582e53045fad465fc4dbe469
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-07-29 22:11:05 +0530

    feat ✨: (main.go) add configed  server

--- prod-api/main.go
+++ prod-api/main.go
@@ -4,20 +4,28 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/cryptus-neoxys/go-micro/prod-api/handlers"
 )
-
 
 func main() {
 	l := log.New(os.Stdout, "prod-api", log.LstdFlags)
 
 	hh := handlers.NewHello(l)
-    gh := handlers.NewGoodbye(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
 	sm.Handle("/goodbye", gh)
 
-    http.ListenAndServe(":9091", sm)
+	s := &http.Server{
+		Addr:         ":9091",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
